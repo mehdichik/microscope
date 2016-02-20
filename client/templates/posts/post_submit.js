00aa9a386bd1e51100c8012fd3b835c01c112c99
Mehdi Ibn Cheikh <mehdi.ibncheikh@esprit.tn>
2016-02-20 02:49:06 +0100

    Notifications

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,12 @@
+Template.postSubmit.created = function() { Session.set('postSubmitErrors', {});
+}
+
+Template.postSubmit.helpers({ errorMessage: function(field) {
+return Session.get('postSubmitErrors')[field]; },
+errorClass: function (field) {
+return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+} });
+
 Template.postSubmit.events({ 'submit form': function(e) {
     e.preventDefault();
 var post = {
@@ -13,11 +22,4 @@ if (result.postExists)
 throwError('This link has already been posted');
       Router.go('postPage', {_id: result._id});
     });
-} });
-Template.postSubmit.created = function() { Session.set('postSubmitErrors', {});
-}
-Template.postSubmit.helpers({ errorMessage: function(field) {
-return Session.get('postSubmitErrors')[field]; },
-errorClass: function (field) {
-return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
 } });
