41dcf80308e40597b21bac5670d239049d235b78
Mehdi Ibn Cheikh <mehdi.ibncheikh@esprit.tn>
2016-03-01 13:44:33 +0100

    Github Authentication
    Github Authentication with OAuth

--- client/templates/comments/comment_item.js
+++ client/templates/comments/comment_item.js
@@ -1,3 +1,13 @@
 Template.commentItem.helpers({ submittedText: function() {
 return this.submitted.toString(); }
 });
+Template.commentItem.events({
+  'change .myFileInput': function(event, template) {
+    var files = event.target.files;
+    for (var i = 0, ln = files.length; i < ln; i++) {
+      Images.insert(files[i], function (err, fileObj) {
+        // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+      });
+    }
+  }
+});
--- server/admin.js
+++
@@ -1,5 +0,0 @@
-var mehdi = Meteor.users.findOne({username: 'mehdichik'});
-Roles.addUserToRoles( mehdi._id ,  ["admin"] );
-
-var nameIcantSpel = Meteor.users.findOne({username: 'tom'});
-Roles.removeUserFromRoles( nameIcantSpel._id, ["admin"] );
