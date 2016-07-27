a5a03e800164eecc0b22465c1a1d229da7f72795
Mehdi Ben Cheikh <mehdi.ibncheikh@esprit.tn>
2016-07-27 17:01:27 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,2 +1,2 @@
-process.env.MAIL_URL = "smtp://mehdi.microscope%40gmail.com:mallouka95@smtp.googlemail.com:587";
+process.env.MAIL_URL = "smtp://mehdi.microscope%40gmail.com:********5@smtp.googlemail.com:587";
 // process.env.MAIL_URL = "smtp://no-reply%40meteor.com:9UZgZic9RI8Ke87dze6E1g@smtp.mandrillapp.com:587";
